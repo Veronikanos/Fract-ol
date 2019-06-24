029327d1a610978e668501fc589d5de3698d1e70
vtlostiu <vveronikanika@gmail.com>
2019-06-24 21:57:07 +0300

    Test multithreads (drawing) 2

--- src/draw_fractals.c
+++ src/draw_fractals.c
@@ -6,7 +6,7 @@
 /*   By: vtlostiu <vtlostiu@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 19:40:15 by vtlostiu          #+#    #+#             */
-/*   Updated: 2019/06/24 21:44:45 by vtlostiu         ###   ########.fr       */
+/*   Updated: 2019/06/24 21:55:45 by vtlostiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,81 +78,104 @@ static void		draw_part(t_pix *pix, size_t y_from, size_t y_to)
 	}
 }
 
-static void		draw_n_parts(t_pix *pix, size_t n_of_sectors)
-{
-	int i;
-
-	i = 0;
-	while( i < n_of_sectors) {
-//		pid_t newp;
+//static void		draw_n_parts(t_pix *pix, size_t n_of_sectors)
+//{
+//	int i;
 //
-//		newp = fork();
-//		if (newp < 0)
-//			errors_msg(1);
-//		if (newp == 0)
-//		{
-			size_t shift = (HEIGHT/n_of_sectors) * i;
-			draw_part(pix, UINT64_MAX + shift, (HEIGHT/n_of_sectors) + shift);
-//		}
-		i++;
-	}
-
-}
-
-
-void draw_fork(t_pix *pix, size_t from, size_t to) {
-	pid_t	newp;
-	newp = fork();
-	if (newp < 0)
-		errors_msg(5);
-	if (newp == 0) {
-		write(1, "Drawing....\n", 12);
-		draw_part(pix, from, to);
-		return;
-	}
-
-}
+//	i = 0;
+//	while( i < n_of_sectors) {
+////		pid_t newp;
+////
+////		newp = fork();
+////		if (newp < 0)
+////			errors_msg(1);
+////		if (newp == 0)
+////		{
+//			size_t shift = (HEIGHT/n_of_sectors) * i;
+//			draw_part(pix, UINT64_MAX + shift, (HEIGHT/n_of_sectors) + shift);
+////		}
+//		i++;
+//	}
+//
+//}
 
 
-static void		draw_fract(t_pix *pix)
+//void draw_fork(t_pix *pix, size_t from, size_t to) {
+//	pid_t	newp;
+//	newp = fork();
+//	if (newp < 0)
+//		errors_msg(5);
+//	if (newp == 0) {
+//		write(1, "Drawing....\n", 12);
+//		draw_part(pix, from, to);
+//		return;
+//	}
+//
+//}
+
+
+//static void		draw_fract(t_pix *pix)
+//{
+//	size_t  n_of_sectors;
+//	int i;
+//	size_t shift;
+//
+//	i = 1;
+//	n_of_sectors = 2;
+////	draw_n_parts(pix, 5);
+//
+//	shift = (HEIGHT/n_of_sectors) * i;
+//	draw_fork(pix, UINT64_MAX + shift, (HEIGHT/n_of_sectors) + shift);
+//	while( i < n_of_sectors) {
+//		shift = (HEIGHT/n_of_sectors) * i;
+//		draw_fork(pix, UINT64_MAX + shift, (HEIGHT/n_of_sectors) + shift);
+//		i++;
+//	}
+//
+//}
+
+void				draw_fork_screen(t_pix *pix, int i, size_t n_of_sectors)
 {
-	size_t  n_of_sectors;
-	int i;
+	pid_t	newp;
 	size_t shift;
 
-	i = 1;
-	n_of_sectors = 2;
-//	draw_n_parts(pix, 5);
-
-	shift = (HEIGHT/n_of_sectors) * i;
-	draw_fork(pix, UINT64_MAX + shift, (HEIGHT/n_of_sectors) + shift);
-	while( i < n_of_sectors) {
+	newp = fork();
+	if (newp < 0)
+		errors_msg(4);
+	if (newp > 0)
+	{
+		mlx_clear_window(pix->mlx_ptr, pix->win_ptr);
 		shift = (HEIGHT/n_of_sectors) * i;
-		draw_fork(pix, UINT64_MAX + shift, (HEIGHT/n_of_sectors) + shift);
+		draw_part(pix, UINT64_MAX + shift, (HEIGHT/n_of_sectors) + shift);
+//	errors_msg(5);
+		mlx_put_image_to_window(pix->mlx_ptr, pix->win_ptr, pix->img_ptr, 0, 0);
+		clear_img(pix);
+		mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 30, COLOR,
+					   "EXIT               ESC");
+		mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 50, COLOR,
+					   "CHANGE FRACTAL     S");
+		mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 70, COLOR,
+					   "CHANGE COLOR       C");
+		mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 90, COLOR,
+					   "ZOOM               MOUSE SCROLL (UP, DOWN)");
+		mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 110, COLOR,
+					   "MOVE               ARROWS: LEFT, RIGHT");
+		mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 130, COLOR,
+					   "ADD ITERATIONS     I");
+		mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 150, COLOR,
+					   "RESET              R");
+	}
+}
+
+void draw_screen(t_pix *pix) {
+	int i ;
+	size_t n_forks;
+
+	i = 0;
+	n_forks = 3;
+	while (i < n_forks) {
+		draw_fork_screen(pix, i, 3);
 		i++;
 	}
 
-}
-
-void				draw_screen(t_pix *pix)
-{
-	mlx_clear_window(pix->mlx_ptr, pix->win_ptr);
-		draw_fract(pix);
-//	errors_msg(5);
-	mlx_put_image_to_window(pix->mlx_ptr, pix->win_ptr, pix->img_ptr, 0, 0);
-	clear_img(pix);
-	mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 30, COLOR,
-				   "EXIT               ESC");
-	mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 50, COLOR,
-				   "CHANGE FRACTAL     S");
-	mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 70, COLOR,
-				   "CHANGE COLOR       C");
-	mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 90, COLOR,
-				   "ZOOM               MOUSE SCROLL (UP, DOWN)");
-	mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 110, COLOR,
-				   "MOVE               ARROWS: LEFT, RIGHT");
-	mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 130, COLOR,
-				   "ADD ITERATIONS     I");
-	mlx_string_put(pix->mlx_ptr, pix->win_ptr, 30, 150, COLOR,
-				   "RESET              R");
 }
